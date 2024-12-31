1cca92a586364df95831b14757142cbba1f4d27c
xroger1969 <c.vasconcelos1969@gmail.com>
2024-12-31 08:46:31 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("reveal-btn").addEventListener("click", function() {
+    document.getElementById("message-box").style.display = "block";
+    this.style.display = "none";
+});
